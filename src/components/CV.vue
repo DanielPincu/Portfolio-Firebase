fce8c7d80c5541608b9a7c525025e7e68eab89a4
DanielPincu <ghepin01@easv365.dk>
2024-04-11 17:05:27 +0200

    updated

--- src/components/CV.vue
+++ src/components/CV.vue
@@ -23,17 +23,17 @@
 
 
   <div class="pt-10">
-      <h1 class="font-bold text-2xl text-center pt-5">Languages</h1>
+      <h1 class="font-bold text-2xl text-center pt-5" data-aos="fade-up">Languages</h1>
       <div v-for="item in languages" class="crd-blue crd-red shadow-xl border-2 border-blue-300 dark:border-red-300 mx-5 my-5 flex flex-col bg-blue-200 dark:bg-red-200 rounded-full items-center h-16 justify-center" data-aos="fade-up" data-aos-duration="2000">
         <h1 class="text-xl font-bold px-4">{{ item.line1 }}</h1>
         <div class="w-36 xl:w-56 2xl:w-64 bg-blue-300 dark:bg-red-300 rounded-full">
           <div class="bg-blue-500 dark:bg-red-500 text-xs font-bold text-center text-white p-1 leading-none rounded-full bg-gradient-to-r from-blue-400 to-blue-600 dark:from-red-400 dark:to-red-600 shadow-lg border-b-2 border-slate-50" :style="{ width: item.line2 + '%' }">{{ item.line2 }}%</div>
         </div>
       </div>
-    </div>
+  </div>
   
     <div class="pt-10">
-      <h1 class="font-bold text-2xl text-center pt-5">Skills</h1>
+      <h1 class="font-bold text-2xl text-center pt-5" data-aos="fade-up">Skills</h1>
       <div v-for="item in skills" class="crd-blue crd-red shadow-xl border-2 border-blue-300 dark:border-red-300 mx-5 my-5 flex flex-col bg-blue-200 dark:bg-red-200 rounded-full items-center h-16 justify-center" data-aos="fade-up" data-aos-duration="2000">
         <h1 class="text-xl font-bold px-4">{{ item.line1 }}</h1>
         <div class="w-36 xl:w-56 2xl:w-64 bg-blue-300 dark:bg-red-300 rounded-full">
